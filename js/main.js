d6b371ace9cdb75ae8bbbf1782efdeba385c17b1
Jocee Paul <joceepaul@me.com>
2023-04-05 18:24:22 +0200

    new version

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const buttonShowMenu = document.getElementById("buttonShowMenu");
+const buttonHideMenu = document.getElementById("buttonHideMenu");
+
+const aside = document.getElementById("aside");
+
+buttonShowMenu.addEventListener("click", function () {
+  aside.classList.remove("hidden");
+});
