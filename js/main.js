5ae6b3754f3b115be9f73c3f62dc50e6979cf084
Jocee Paul <joceepaul@me.com>
2023-04-07 20:28:00 +0200

    fix function to put destinations, adjust style, add event listener to addButton

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ let destinations = [
     imageSrc:
       "http://127.0.0.1:5500/travel-blog/img/marocco-marrakech-boucharouite-closeup.jpg",
     titel: "Marrakech–– most beautiful traditional way of Upcycling",
-    location: "Marrakech",
+    link: "http://127.0.0.1:5500/travel-blog/marocco.html",
     country: "Marocco",
     date: "march 2018",
   },
@@ -32,22 +32,59 @@ let destinations = [
   },
 ];
 
+// Event Listeners
+
+addButton.addEventListener("click", function () {
+  const newDestination = {
+    imageSrc: imageInput.value,
+    location: inputCity.value,
+    titel: inputTitle.value,
+    country: inputCountry.value,
+    date: inputDate.value,
+  };
+  console.log(newDestination);
+  destinations.push(newDestination);
+});
+
 // Functions
 
-function filldestinationContainer() {
-  const destinationContainer = document.querySelector(".destinationContainer");
+const destinationContainer = document.querySelector(".destinationContainer");
 
-  destinationContainer.innerHtml = "";
-  for (let i = 0; i < destinations.length; i++) {
-    const newDestination = `
-        <div class="destination">
-            <img src="${destinations[i].imageSrc}">
-            <h1>"${destinations[i].titel}"</h1>
-            <p>"${destinations[i].country}"</p>
-            <p>"${destinations[i].date}"</p>
-        </div>`;
-    // console.log(newDestination);
-    destinationContainer.innerHtml = newDestination;
-  }
-}
-filldestinationContainer();
+destinationContainer.innerHTML = destinations
+  .map(
+    (destinations) => ` <div class="destination">
+        <a href="${destinations.link}"
+        ><img
+          id="imgHigh"
+          src="${destinations.imageSrc}"
+          alt="Boucharouite closeup"
+      /></a>
+        <a href="${destinations.link}"
+        ><h1>"${destinations.titel}"</h1></a
+      >
+        <p class="country">
+        <a href="${destinations.link}">"${destinations.country}"</a>
+      </p>
+        <p class="date">"${destinations.date}"</p>
+        <div id="divider" class="sectionDivider"></div>
+        </div>`
+  )
+  .join("");
+
+// function filldestinationContainer() {
+//   const destinationContainer = document.querySelector(".destinationContainer");
+
+// //   destinationContainer.innerHtml = "";
+//   for (let i = 0; i < destinations.length; i++) {
+//     const newDestination = `
+//         <div class="destination">
+//             <img src="${destinations[i].imageSrc}">
+//             <h1>"${destinations[i].titel}"</h1>
+//             <p>"${destinations[i].country}"</p>
+//             <p>"${destinations[i].date}"</p>
+//         </div>`;
+//     // console.log(newDestination);
+//     destinationContainer.innerHtml = newDestination;
+//   }
+// }
+// filldestinationContainer();
