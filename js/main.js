be60e11334c956bf935dad6907fc68a5ccc36070
Jocee Paul <joceepaul@me.com>
2023-04-12 12:39:58 +0200

    fetch weather and event listener to print the weather data into the html works

--- js/main.js
+++ js/main.js
@@ -1,3 +1,93 @@
+// FUNCTION ON TOP
+
+function filldestinationContainer() {
+  const destinationContainer = document.querySelector(".destinationContainer");
+  destinationContainer.innerHtml = "";
+
+  destinationContainer.innerHTML = destinations
+    .map(
+      // Helder: added data-city to the `div` so we know with ease which city was clicked
+      // removed all the href from `a` tags since they were not being used, and were causing issues
+      (
+        destinations
+      ) => ` <div class="destination" data-city="${destinations.location}">
+        <a
+        ><img
+          src=${destinations.imageSrc}
+        
+      /></a>
+        <a
+        ><h1>${destinations.location}</h1></a
+      >
+        <p class="country">
+        <a>${destinations.country}</a>
+      </p>
+        <p class="date">${destinations.date}</p>
+
+        </div>`
+    )
+    .join("");
+}
+
+function saveToLocalStorage() {
+  const myJsonString = JSON.stringify(destinations);
+  localStorage.setItem("Destinations", myJsonString);
+}
+
+function loadFromLocalStorage() {
+  const myJsonString = localStorage.getItem("Destinations");
+  if (myJsonString) {
+    destinations = JSON.parse(myJsonString);
+  }
+}
+
+function deleteFromLocalStorage() {
+  localStorage.removeItem("Destinations");
+}
+
+// WEATHER API
+
+function updateListeners() {
+  // Helder: find all `divs` with the class destination so we can loop through them
+  // find the cityName based on the data-city element and then add a click listener to update
+  // weatherMap info
+  const destinationCards = document.querySelectorAll(".destination");
+
+  console.log(destinationCards);
+
+  for (let i = 0; i < destinationCards.length; i++) {
+    // Helder: because we added data-city on the HTML we can now easily read the city from the div attribute
+    // (data-city in HTML can be read as dataset.city in Javascript)
+    // const cityName = destinationCards[i].dataset.city;
+
+    const currentDestination = destinations[i];
+    console.log(currentDestination);
+    const cityName = currentDestination.location;
+
+    destinationCards[i].addEventListener("click", function () {
+      getWeatherData(cityName);
+      // alert(currentDestination);
+      destinationsBox.innerHTML = `<p>${currentDestination.location}</p><h2>${currentDestination.titel}</h2>`;
+    });
+  }
+}
+
+function getWeatherData(cityName) {
+  const apiKey = "4d52d38088073627a3b3628993c298c0";
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+  fetch(URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      console.log(result);
+      weatherBox.innerHTML = `The weather in ${cityName} is ${result.main.temp}`;
+    });
+}
+
+// ASIDE MENU
+
 const buttonShowMenu = document.getElementById("buttonShowMenu");
 const buttonHideMenu = document.getElementById("buttonHideMenu");
 
@@ -11,7 +101,7 @@ buttonHideMenu.addEventListener("click", function () {
   aside.classList.add("hidden");
 });
 
-// Original Array
+// DEFAULT LOCATIONS (ORIGINAL ARRAY)
 
 let destinations = [
   {
@@ -32,7 +122,7 @@ let destinations = [
   },
 ];
 
-// Event Listeners
+// EVENT LISTENER FOR FORM
 
 addButton.addEventListener("click", function () {
   const newDestination = {
@@ -44,85 +134,147 @@ addButton.addEventListener("click", function () {
   };
 
   destinations.push(newDestination);
-  filldestinationContainer();
   saveToLocalStorage();
+  filldestinationContainer();
+
+  // Helder: update the listeners so the newly added cities can be clicked for weather Info too
+  updateListeners();
 });
 
-// Functions
+// Helder: moved weatherBox here, so it can be accessed in all functions
+const weatherBox = document.querySelector("#weatherBox");
+const destinationsBox = document.querySelector("#destinationsBox");
 
-function filldestinationContainer() {
-  const destinationContainer = document.querySelector(".destinationContainer");
-  destinationContainer.innerHtml = "";
-
-  destinationContainer.innerHTML = destinations
-    .map(
-      (destinations) => ` <div class="destination">
-        <a href=""
-        ><img
-          src=${destinations.imageSrc}
-        
-      /></a>
-        <a href=""
-        ><h1>${destinations.location}</h1></a
-      >
-        <p class="country">
-        <a href=${destinations.link}>${destinations.country}</a>
-      </p>
-        <p class="date">${destinations.date}</p>
-
-        </div>`
-    )
-    .join("");
-}
-filldestinationContainer();
-
-function saveToLocalStorage() {
-  const myJsonString = JSON.stringify(destinations);
-  localStorage.setItem("Destinations", myJsonString);
-}
-
-function loadFromLocalStorage() {
-  const myJsonString = localStorage.getItem("Destinations");
-  if (myJsonString) {
-    destinations = JSON.parse(myJsonString);
-  }
-}
-
+// APPLICATION START
 loadFromLocalStorage();
 filldestinationContainer();
+updateListeners();
 
-function deleteFromLocalStorage() {
-  localStorage.removeItem("Destinations");
-}
+// const buttonShowMenu = document.getElementById("buttonShowMenu");
+// const buttonHideMenu = document.getElementById("buttonHideMenu");
 
-// WEATHER API
+// const aside = document.getElementById("aside");
 
-const weatherBox = document.querySelector("#weatherBox");
+// buttonShowMenu.addEventListener("click", function () {
+//   aside.classList.remove("hidden");
+// });
 
-for (let i = 0; i < destinations.length; i++) {
-  const cityAI = destinations[i].location;
-  console.log(cityAI);
+// buttonHideMenu.addEventListener("click", function () {
+//   aside.classList.add("hidden");
+// });
 
-  cityAI.addEventListener("click", function () {
-    const cityName = cityAI.innerHTML;
+// // Original Array
 
-    getWeatherData(cityName);
-  });
-}
+// let destinations = [
+//   {
+//     imageSrc:
+//       "http://127.0.0.1:5500/travel-blog/img/marocco-marrakech-boucharouite-closeup.jpg",
+//     titel: "Marrakech–– most beautiful traditional way of Upcycling",
+//     location: "Marrakech",
+//     country: "Marocco",
+//     date: "march 2018",
+//   },
+//   {
+//     imageSrc:
+//       "http://127.0.0.1:5500/travel-blog/img/france-paris-deco-off-bistro-detail.jpg",
+//     titel: "Paris–– colorful patters at Paris Deco Off",
+//     location: "Paris",
+//     country: "France",
+//     date: "january 2019",
+//   },
+// ];
 
-function getWeatherData(cityName) {
-  const apiKey = "4d52d38088073627a3b3628993c298c0";
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+// // Event Listeners
 
-  fetch(URL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (result) {
-      console.log(result);
-      weatherBox.innerHTML = `The weather in ${cityName} is ${result.main.temp}`;
-    });
-}
+// addButton.addEventListener("click", function () {
+//   const newDestination = {
+//     imageSrc: imageInput.value,
+//     titel: inputTitle.value,
+//     location: inputCity.value,
+//     country: inputCountry.value,
+//     date: inputDate.value,
+//   };
+
+//   destinations.push(newDestination);
+//   filldestinationContainer();
+//   saveToLocalStorage();
+// });
+
+// // Functions
+
+// function filldestinationContainer() {
+//   const destinationContainer = document.querySelector(".destinationContainer");
+//   destinationContainer.innerHtml = "";
+
+//   destinationContainer.innerHTML = destinations
+//     .map(
+//       (destinations) => ` <div class="destination">
+//         <a href=""
+//         ><img
+//           src=${destinations.imageSrc}
+
+//       /></a>
+//         <a href=""
+//         ><h1>${destinations.location}</h1></a
+//       >
+//         <p class="country">
+//         <a href=${destinations.link}>${destinations.country}</a>
+//       </p>
+//         <p class="date">${destinations.date}</p>
+
+//         </div>`
+//     )
+//     .join("");
+// }
+// filldestinationContainer();
+
+// function saveToLocalStorage() {
+//   const myJsonString = JSON.stringify(destinations);
+//   localStorage.setItem("Destinations", myJsonString);
+// }
+
+// function loadFromLocalStorage() {
+//   const myJsonString = localStorage.getItem("Destinations");
+//   if (myJsonString) {
+//     destinations = JSON.parse(myJsonString);
+//   }
+// }
+
+// loadFromLocalStorage();
+// filldestinationContainer();
+
+// function deleteFromLocalStorage() {
+//   localStorage.removeItem("Destinations");
+// }
+
+// // WEATHER API
+
+// const weatherBox = document.querySelector("#weatherBox");
+
+// for (let i = 0; i < destinations.length; i++) {
+//   const cityAI = destinations[i].location;
+//   console.log(cityAI);
+
+//   cityAI.addEventListener("click", function () {
+//     const cityName = cityAI.innerHTML;
+
+//     getWeatherData(cityName);
+//   });
+// }
+
+// function getWeatherData(cityName) {
+//   const apiKey = "4d52d38088073627a3b3628993c298c0";
+//   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+//   fetch(URL)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (result) {
+//       console.log(result);
+//       weatherBox.innerHTML = `The weather in ${cityName} is ${result.main.temp}`;
+//     });
+// }
 
 // const localWeather = document.querySelector(".weatherApi");
 // fetch(
