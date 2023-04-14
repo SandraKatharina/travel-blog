c1ba96e376cc8d24019c688a1f147daa8ab41138
Jocee Paul <joceepaul@me.com>
2023-04-14 13:41:53 +0200

    try to add a functin to clear the input fields after adding the new destination

--- js/main.js
+++ js/main.js
@@ -41,6 +41,11 @@ function loadFromLocalStorage() {
   }
 }
 
+function clearText() {
+  const allInputFields = document.querySelectorAll(".inputField");
+  allInputFields.value = "";
+}
+
 // WEATHER API
 
 function updateListeners() {
@@ -49,7 +54,7 @@ function updateListeners() {
   // weatherMap info
   const destinationCards = document.querySelectorAll(".destination");
 
-  console.log(destinationCards);
+  // console.log(destinationCards);
 
   for (let i = 0; i < destinationCards.length; i++) {
     // Helder: because we added data-city on the HTML we can now easily read the city from the div attribute
@@ -150,6 +155,7 @@ addButton.addEventListener("click", function () {
 
   // Helder: update the listeners so the newly added cities can be clicked for weather Info too
   updateListeners();
+  clearText();
 });
 
 function deleteLastDestination() {
@@ -173,107 +179,3 @@ const destinationsTitelBox = document.querySelector("#destinationsTitelBox");
 loadFromLocalStorage();
 filldestinationContainer();
 updateListeners();
-
-// // Event Listeners
-
-// addButton.addEventListener("click", function () {
-//   const newDestination = {
-//     imageSrc: imageInput.value,
-//     titel: inputTitle.value,
-//     location: inputCity.value,
-//     country: inputCountry.value,
-//     date: inputDate.value,
-//   };
-
-//   destinations.push(newDestination);
-//   filldestinationContainer();
-//   saveToLocalStorage();
-// });
-
-// // Functions
-
-// function filldestinationContainer() {
-//   const destinationContainer = document.querySelector(".destinationContainer");
-//   destinationContainer.innerHtml = "";
-
-//   destinationContainer.innerHTML = destinations
-//     .map(
-//       (destinations) => ` <div class="destination">
-//         <a href=""
-//         ><img
-//           src=${destinations.imageSrc}
-
-//       /></a>
-//         <a href=""
-//         ><h1>${destinations.location}</h1></a
-//       >
-//         <p class="country">
-//         <a href=${destinations.link}>${destinations.country}</a>
-//       </p>
-//         <p class="date">${destinations.date}</p>
-
-//         </div>`
-//     )
-//     .join("");
-// }
-// filldestinationContainer();
-
-// function saveToLocalStorage() {
-//   const myJsonString = JSON.stringify(destinations);
-//   localStorage.setItem("Destinations", myJsonString);
-// }
-
-// function loadFromLocalStorage() {
-//   const myJsonString = localStorage.getItem("Destinations");
-//   if (myJsonString) {
-//     destinations = JSON.parse(myJsonString);
-//   }
-// }
-
-// loadFromLocalStorage();
-// filldestinationContainer();
-
-// function deleteFromLocalStorage() {
-//   localStorage.removeItem("Destinations");
-// }
-
-// // WEATHER API
-
-// const weatherBox = document.querySelector("#weatherBox");
-
-// for (let i = 0; i < destinations.length; i++) {
-//   const cityAI = destinations[i].location;
-//   console.log(cityAI);
-
-//   cityAI.addEventListener("click", function () {
-//     const cityName = cityAI.innerHTML;
-
-//     getWeatherData(cityName);
-//   });
-// }
-
-// function getWeatherData(cityName) {
-//   const apiKey = "4d52d38088073627a3b3628993c298c0";
-//   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-
-//   fetch(URL)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (result) {
-//       console.log(result);
-//       weatherBox.innerHTML = `The weather in ${cityName} is ${result.main.temp}`;
-//     });
-// }
-
-// const localWeather = document.querySelector(".weatherApi");
-// fetch(
-//   "https://api.openweathermap.org/data/2.5/weather?q=Paris,FR&appid=4d52d38088073627a3b3628993c298c0"
-// )
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     localWeather.innerHTML = data.results
-//       .map((location) => `<li>${location.q}</li>`)
-//       .join("");
-//   });
